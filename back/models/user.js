c79b5a2931ef95ed4434a9672ae15203e965bc4e
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-06-05 13:08:41 -0500

    Updating users models and controllers

--- back/models/user.js
+++ back/models/user.js
@@ -4,7 +4,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  userId: { type: String, required: true, unique: true}
+  email: { type: String, unique: true, lowercase: true },
+  displayName: { type: String },
+  password: { type: String, select: false }
+})
+
+UserSchema.pre('save', function (next) {
+  let user = this
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, null, function (err, hash){
+      if (err) return next(err)
+      user.password = hash
+      next()
+    })
+  })
 })
 
 module.exports = mongoose.model('User', UserSchema)
