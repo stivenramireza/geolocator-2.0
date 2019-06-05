d629479124f4676ff6b9c8df524a31634c9631ad
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-06-05 15:17:38 -0500

    Adding bycript

--- back/models/user.js
+++ back/models/user.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
 const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
